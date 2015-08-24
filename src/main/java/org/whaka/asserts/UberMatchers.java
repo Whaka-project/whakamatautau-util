f05d397cc2eceb3b0e8157d6889d9f87a5fdf13c
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2015-08-24 17:53:07 +0300

    #35 [Assert] Implement UberMatchers front

---
+++ src/main/java/org/whaka/asserts/UberMatchers.java
@@ -0,0 +1,12 @@
+package org.whaka.asserts;
+
+/**
+ * Class provides static factory methods for custom Hamcrest matchers provided by the librabry.
+ * 
+ * @see NumberMatchers
+ */
+public final class UberMatchers {
+
+	private UberMatchers() {
+	}
+}
