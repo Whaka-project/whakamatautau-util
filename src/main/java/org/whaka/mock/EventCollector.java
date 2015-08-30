4e9cdfe84f73c9ede81ea32cfc5aac606088440d
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2015-08-30 13:26:41 +0400

    #75 [Utils] Event collector - partial captor mode
    Factory methods are refactored.

--- src/main/java/org/whaka/mock/EventCollector.java
+++ src/main/java/org/whaka/mock/EventCollector.java
@@ -8,14 +8,13 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import org.mockito.ArgumentCaptor;
-import org.mockito.Matchers;
 import org.mockito.MockSettings;
 import org.mockito.Mockito;
 import org.mockito.invocation.DescribedInvocation;
+import org.whaka.util.reflection.UberClasses;
 
 import com.google.common.collect.ImmutableList;
 
@@ -43,18 +42,10 @@ public class EventCollector<Target, Event> {
 		return create(targetClass, method, Arrays.asList(filters));
 	}
 
-	@SuppressWarnings("unchecked")
 	public static <T, E> EventCollector<T, E> create(Class<T> targetClass,
 			BiConsumer<T, E> method, Collection<Predicate<E>> filters){
-
-		BiConsumer<T, Consumer<E>> eventCapture = (target, c) -> {
-			method.accept(Mockito.doAnswer(invoke -> {
-				c.accept((E) invoke.getArguments()[0]);
-				return null;
-			}).when(target), Matchers.any());
-		};
-		
-		return doCreate(targetClass, eventCapture, filters);
+		return EventCollector.<T, E>createPartial(targetClass, UberClasses.cast(Object.class),
+				(l, c) -> method.accept(l, c.capture()), filters);
 	}
 	
 	@SafeVarargs
@@ -66,24 +57,7 @@ public class EventCollector<Target, Event> {
 	public static <T, E> EventCollector<T, E> createPartial(Class<T> targetClass, Class<E> eventClass,
 			BiConsumer<T, ArgumentCaptor<E>> method, Collection<Predicate<E>> filters){
 
-		BiConsumer<T, Consumer<E>> eventCapture = (target, c) -> {
-			ArgumentCaptor<E> captor = ArgumentCaptor.forClass(eventClass);
-			method.accept(Mockito.doAnswer(invoke -> {
-				c.accept(captor.getValue());
-				return null;
-			}).when(target), captor);
-		};
-		
-		return doCreate(targetClass, eventCapture, filters);
-	}
-	
-	private static <T,E> EventCollector<T, E> doCreate(Class<T> targetClass,
-			BiConsumer<T, Consumer<E>> eventCapture, Collection<Predicate<E>> filters) {
-		
-		List<EventHandler<E>> eventHandlers = filters.stream()
-				.filter(EventHandler.class::isInstance)
-				.map(EventHandler.class::cast)
-				.collect(toList());
+		List<EventHandler<E>> eventHandlers = selectEventHandlers(filters);
 		
 		List<DescribedInvocation> invokes = new ArrayList<>();
 		MockSettings settings = Mockito.withSettings()
@@ -92,7 +66,9 @@ public class EventCollector<Target, Event> {
 		T target = Mockito.mock(targetClass, settings);
 		EventCollector<T, E> collector = new EventCollector<>(target);
 
-		eventCapture.accept(target, event -> {
+		ArgumentCaptor<E> captor = ArgumentCaptor.forClass(eventClass);
+		method.accept(Mockito.doAnswer(invoke -> {
+			E event = captor.getValue();
 			synchronized (collector.lock) {
 				boolean filterFail = stream(filters).map(p -> p.test(event)).toSet().contains(false);
 				if (!filterFail) {
@@ -100,12 +76,20 @@ public class EventCollector<Target, Event> {
 					eventHandlers.forEach(c -> c.eventCollected(event));
 				}
 			}
-		});
+			return null;
+		}).when(target), captor);
 		
 		if (invokes.size() != 1)
 			throw new IllegalStateException("Single listener interaction was expected! But actual: " + invokes);
 		
 		return collector;
+	}
+	
+	private static <E> List<EventHandler<E>> selectEventHandlers(Collection<Predicate<E>> filters) {
+		return filters.stream()
+				.filter(EventHandler.class::isInstance)
+				.map(EventHandler.class::cast)
+				.collect(toList());
 	}
 	
 	public Target getTarget() {
