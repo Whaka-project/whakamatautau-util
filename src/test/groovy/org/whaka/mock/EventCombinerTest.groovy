424ed289f5d4b373748d414bb888e01304a65356
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2015-08-31 19:16:53 +0300

    #76 [Utils] Event collector for multiple arguments
    EventCombiner test cases is implemented.
    Javadoc is added.

---
+++ src/test/groovy/org/whaka/mock/EventCombinerTest.groovy
@@ -0,0 +1,127 @@
+package org.whaka.mock
+
+import java.util.function.BiConsumer
+import java.util.function.Function
+
+import org.whaka.util.function.Consumer3
+import org.whaka.util.function.Consumer4
+
+import spock.lang.Specification
+
+class EventCombinerTest extends Specification {
+
+	def "for captors"() {
+		given:
+			BiConsumer methodCall = Mock()
+			Function combinator = Mock()
+
+		when: "#forCaptors is called with some method call, and a combinator"
+			def combiner = EventCombiner.forCaptors(captors, methodCall, combinator)
+		then: "method call and combinator function are stored"
+			combiner.getMethodCall().is(methodCall)
+			combiner.getCombiner().is(combinator)
+		and: "specified number of argument captors are created"
+			combiner.getCaptors().length == captors
+
+		when: "accept is called with some target"
+			combiner.accept("target")
+		then: "specified method call is called with the target and created captors"
+			1 * methodCall.accept("target", combiner.getCaptors())
+
+		where:
+			captors	<< [1, 3, 5]
+	}
+
+	def "for values"() {
+		given:
+			BiConsumer methodCall = Mock()
+			Function combinator = Mock()
+
+		when: "#forValues is called with some method call, and a combinator"
+			def combiner = EventCombiner.forValues(captors, methodCall, combinator)
+		then: "combinator function is stored"
+			combiner.getCombiner().is(combinator)
+		and: "method call is wrapped, so it's not the same"
+			combiner.getMethodCall().is(methodCall) == false
+		and: "specified number of argument captors is created"
+			combiner.getCaptors().length == captors
+
+		when: "accept is called with some target"
+			combiner.accept("target")
+		then: "method call is called with the target and initiated matchers"
+			1 * methodCall.accept("target", _) >> {
+				Object[] matchers = it[1]
+				// the same amount of matchers is passed, as created captors
+				assert matchers.size() == captors
+				matchers.each {
+					// each matcher is null
+					assert it == null
+				}
+			}
+
+		where:
+			captors	<< [1, 3, 5]
+	}
+
+	def "for captor"() {
+		given:
+			BiConsumer methodCall = Mock()
+
+		when: "#forCaptor is called with some method call"
+			def combiner = EventCombiner.forCaptor(methodCall)
+		then: "method call is wrapped, so it's not the same"
+			combiner.getMethodCall().is(methodCall) == false
+		and: "single argument captor is created"
+			combiner.getCaptors().length == 1
+
+		when: "accept is called with some target"
+			combiner.accept("target")
+		then: "specified method call is called with the target and single created captor"
+			1 * methodCall.accept("target", combiner.getCaptors()[0])
+	}
+
+	def "create: BiConsumer"() {
+		given:
+			BiConsumer methodCall = Mock()
+
+		when:
+			def combiner = EventCombiner.create(methodCall)
+		then:
+			combiner.getCaptors().length == 1
+
+		when:
+			combiner.accept("target")
+		then:
+			1 * methodCall.accept("target", null)
+	}
+
+	def "create: Consumer3"() {
+		given:
+			Consumer3 methodCall = Mock()
+
+		when:
+			def combiner = EventCombiner.create(methodCall)
+		then:
+			combiner.getCaptors().length == 2
+
+		when:
+			combiner.accept("target")
+		then:
+			1 * methodCall.accept("target", null, null)
+	}
+
+	def "create: Consumer4"() {
+		given:
+			Consumer4 methodCall = Mock()
+
+		when:
+			def combiner = EventCombiner.create(methodCall)
+		then:
+			combiner.getCaptors().length == 3
+
+		when:
+			combiner.accept("target")
+		then:
+			1 * methodCall.accept("target", null, null, null)
+	}
+}
---
+++ src/test/java/org/whaka/mock/EventCombinerJavaTest.java
@@ -0,0 +1,67 @@
+package org.whaka.mock;
+
+import static org.hamcrest.Matchers.*;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.whaka.asserts.Assert;
+import org.whaka.util.reflection.UberClasses;
+
+
+@RunWith(JUnit4.class)
+public class EventCombinerJavaTest {
+
+	/**
+	 * <p>{@link EventCombiner} only encapsulates a call to an abstract instance,
+	 * with automatically created argument captors, as parameters. But it doesn't perform any additional mock
+	 * configuration, and - in fact - doesn't know anything about any instance at all; so mock configuration got
+	 * to be done manually.
+	 * 
+	 * <p>Method {@link EventCombiner#getValue()} calls {@link ArgumentCaptor#getValue()} on all generated
+	 * captors and then apply combinator function to received results. So returned value is the result
+	 * of the combinator specified at the construction of the combiner.
+	 */
+	@Test
+	public void test_getValue() {
+
+		// given:
+		
+		Function<Object[], String> combinator = Mockito.mock(UberClasses.cast(Function.class));
+		Mockito.when(combinator.apply(Matchers.any())).then(invoke -> {
+			Object[] arr = invoke.getArgumentAt(0, Object[].class);
+			Assert.assertThat(arr, arrayWithSize(2));
+			Assert.assertThat(arr[0], equalTo("qwe"));
+			Assert.assertThat(arr[1], equalTo(42));
+			return "combined result";
+		});
+		
+		BiConsumer<Listener, ArgumentCaptor<?>[]> methodCall = (target, captors) ->
+			target.event2((String)captors[0].capture(), (Integer)captors[1].capture());
+			
+		EventCombiner<Listener, String> combiner = EventCombiner.forCaptors(2, methodCall, combinator);
+
+		Listener target = Mockito.mock(Listener.class);
+		combiner.accept(Mockito.doNothing().when(target));
+		
+		// when:
+
+		target.event2("qwe", 42);
+		String res = combiner.getValue();
+		
+		// then:
+		
+		Mockito.verify(combinator, Mockito.times(1)).apply(Matchers.any());
+		Assert.assertThat(res, equalTo("combined result"));
+	}
+	
+	public static interface Listener {
+		void event2(String s, Integer i);
+	}
+}
